121b586b1760effe049ed109458f89845de638e3
Meet Sable <meetsable@gmail.com>
2022-01-20 23:10:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,6 @@ const port = process.env.PORT || 5000;
 
 app.set('view engine', 'pug');
 
-app.use(function (req, res, next) {
-  res.setHeader(
-    'Content-Security-Policy',
-    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
-  );
-  next();
-});
-
-
 app.use('/', express.static('public'));
 
 
